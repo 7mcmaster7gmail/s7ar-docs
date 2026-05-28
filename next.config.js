2a29210d33d244334b202db3c2ee0bcee4f64fea
7mcmaster7gmail <7mcmaster7@gmail.com>
2026-05-28 11:42:03 +0800

    Remove deprecated swcMinify option from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js', 'mdx', 'md'],
-  webpack: (config, { isServer }) => {
-    return config;
-  },
 };
 
 module.exports = nextConfig;
